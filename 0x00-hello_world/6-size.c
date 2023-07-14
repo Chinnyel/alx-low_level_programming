d726736e6a88bfc6758a150ff0edd322a131f975
chinnyel <wigwechi8@gmail.com>
2023-07-14 13:01:17 -0700

    6 size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,12 +2,12 @@
 /**
  * main - compilation gcc
  *
- * Return: Always 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
 	printf("size of a char: %lu byte(s)\n", sizeof(char));
-	printf("size of an int: %lu bye(s)\n", sizeof(int));
+	printf("size of an int: %lu byte(s)\n", sizeof(int));
 	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("size of a float: %lu byte(s)\n", sizeof(float));
