aec94c743adac2b7e2a44113909eb9b095066acb
chinnyel <wigwechi8@gmail.com>
2023-07-21 00:06:10 -0700

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _isupper - functio that checks for uppercase character
+ * @c: int type
+ * Return: return 1 if c is uppercase and 0 if otherwise
+ */
+int _isupper(int c)
+{
+	int c = 0;
+	int i = 0;
+
+	while (c < 97 || c <= 122)
+		c++;
+	{
+		_putchar(c);
+
+		return (0);
+	}
+	while (i < 65 || i <= 90)
+		i++;
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (1);
+}
