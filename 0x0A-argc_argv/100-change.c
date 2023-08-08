c6fc0436b2fffa2a60943f74413236832ef62881
chinnyel <wigwechi8@gmail.com>
2023-08-08 11:07:05 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,1 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that prints minimum no of coins to mk chng 
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0 if no errors, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int a, n = 0, i, t;
+	int c[5] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	if (a <= 0)
+	{
+		puts("0");
+		return (1);
+	}
+	else
+	{
+		for (i = 0; i < 5; i++)
+		{
+			t = a / c[i];
+			a -= t * c[i];
+			n += t;
+			if (a == 0)
+				break;
+		}
+	}
+	printf("%d\n", n);
+	return (0);
+}
