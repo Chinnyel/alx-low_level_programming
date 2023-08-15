5ba72a36359eddc21ab98bede4b503fd31f0e892
chinnyel <wigwechi8@gmail.com>
2023-08-15 12:06:51 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * File:3 function like macro
+ * Description: header file that defines function like macro
+ * ABS(x) that computes the absolute value of a number x
+ */
+
+#define ABS(X) (x > 0 ? (x) : (x) * -1)
+
+#endif
---
+++ 0x0D-preprocessor/3-main.c
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/**
+ * file: function like macro
+ * Description: header file that defines function like macro
+ * ABS(X)
+ */
+
+#define ABS(X) (x > 0 ? (x) : (x) * -1)
+
+#endif
+typedef int make_iso_happy;
